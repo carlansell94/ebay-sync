carlansell94/ebay-sync

commit 2c72291e1c8aee7eebf0c66ece42e0f1509f3ec9

Rename fulfillment to address to avoid conflict with eBay fulfillment API functions

sync/getSales.py
@@ -4,7 +4,7 @@ from core.sales import Sales
 from model.sale import Sale
 from model.line import Line
 from model.payment import Payment
-from model.fulfillment import Fulfillment
+from model.address import Address
 from model.transaction import Transaction
 from datetime import datetime
 from model.fee import Fee
@@ -36,20 +36,20 @@ class getSales:
             self.sale = Sale(self.db)
             self.line = Line(self.db)
             self.payment = Payment(self.db)
-            self.fulfillment = Fulfillment(self.db)
+            self.address = Address(self.db)
             self.transaction = Transaction(self.db)
 
             order_id = sale['orderId'].split('!')[0]
             self.sale.setOrderId(order_id)
             self.line.setOrderId(order_id)
-            self.fulfillment.setOrderId(order_id)
+            self.address.setOrderId(order_id)
             self.sale.setLastUpdated(sale['lastModifiedDate'])
 
             if self.syncNeeded():
                 self.sale.setLegacyOrderId(sale['legacyOrderId'])
                 self.sale.setSaleDate(sale['creationDate'])
                 self.sale.setBuyerUsername(sale['buyer']['username'])
-                self.sale.setStatus(sale['orderFulfillmentStatus'])
+                self.sale.setStatus(sale['orderAddressStatus'])
                 self.sale.add()
                 
                 self.fee = Fee(self.db)
@@ -64,8 +64,8 @@ class getSales:
                     self.line.setTitle(line_items['title'])
                     self.line.setSaleFormat(line_items['soldFormat'])
                     self.line.setQuantity(line_items['quantity'])
-                    self.line.setFulfillmentStatus(line_items
-                        ['lineItemFulfillmentStatus']
+                    self.line.setAddressStatus(line_items
+                        ['lineItemAddressStatus']
                     )
                     self.line.add()
 
@@ -99,23 +99,23 @@ class getSales:
                     self.payment.setTransactionId(transaction_id)        
                     self.payment.add()
 
-                for shipping in sale['fulfillmentStartInstructions']:
+                for shipping in sale['AddressStartInstructions']:
                     ship_to = shipping['shippingStep']['shipTo']
-                    self.fulfillment.setBuyerName(ship_to['fullName'])
-                    self.fulfillment.setAddressLine1(ship_to['contactAddress']
+                    self.address.setBuyerName(ship_to['fullName'])
+                    self.address.setAddressLine1(ship_to['contactAddress']
                         ['addressLine1']
                     )
-                    self.fulfillment.setCity(ship_to['contactAddress']['city'])
+                    self.address.setCity(ship_to['contactAddress']['city'])
 
                     try:
-                        self.fulfillment.setCounty(ship_to['contactAddress']['stateOrProvince'])
+                        self.address.setCounty(ship_to['contactAddress']['stateOrProvince'])
                     except KeyError:
-                        self.fulfillment.setCounty("")
+                        self.address.setCounty("")
 
-                    self.fulfillment.setPostCode(ship_to['contactAddress']
+                    self.address.setPostCode(ship_to['contactAddress']
                         ['postalCode']
                     )
-                    self.fulfillment.setCountryCode(ship_to['contactAddress']
+                    self.address.setCountryCode(ship_to['contactAddress']
                         ['countryCode']
                     )
-                    self.fulfillment.add()
+                    self.address.add()

model/address.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-class Fulfillment():
+class Address():
     def __init__(self, db):
         self.db = db
 
@@ -34,7 +34,7 @@ class Fulfillment():
 
     def add(self):
         query = self.db.cursor()
-        query.execute("""INSERT INTO fulfillment (order_id, buyer_name,
+        query.execute("""INSERT INTO addresses (order_id, buyer_name,
                         address_line_1, city, county, post_code, country_code)
                         VALUES(%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY
                         UPDATE order_id = VALUES(order_id)""",

setup/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE `feedback` (
   `comment` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `fulfillment` (
+CREATE TABLE `address` (
   `order_id` varchar(26) NOT NULL,
   `buyer_name` varchar(64) NOT NULL,
   `address_line_1` varchar(64) NOT NULL,
@@ -84,7 +84,7 @@ ALTER TABLE `feedback`
   ADD PRIMARY KEY (`feedback_id`),
   ADD KEY `feedback_legacy_order_id` (`legacy_order_id`);
 
-ALTER TABLE `fulfillment`
+ALTER TABLE `address`
   ADD PRIMARY KEY (`order_id`);
 
 ALTER TABLE `line`
@@ -113,7 +113,7 @@ ALTER TABLE `fee`
 ALTER TABLE `feedback`
   ADD CONSTRAINT `feedback_legacy_order_id` FOREIGN KEY (`legacy_order_id`) REFERENCES `sale` (`legacy_order_id`) ON UPDATE CASCADE;
 
-ALTER TABLE `fulfillment`
+ALTER TABLE `address`
   ADD CONSTRAINT `fulfillment_order_id` FOREIGN KEY (`order_id`) REFERENCES `sale` (`order_id`) ON UPDATE CASCADE;
 
 ALTER TABLE `payment`

commit 05f6b3b7c8f14bc70bcd9932d0f8fa5edd125acf

Fix incorrect array indeces introduced in 2c72291 (Rename fulfillment to address to avoid conflict with eBay fulfillment API functions, 2022-03-18)

sync/getSales.py
@@ -66,7 +66,7 @@ class getSales:
                 self.sale.setLegacyOrderId(sale['legacyOrderId'])
                 self.sale.setSaleDate(sale['creationDate'])
                 self.sale.setBuyerUsername(sale['buyer']['username'])
-                self.sale.setStatus(sale['orderAddressStatus'])
+                self.sale.setStatus(sale['orderFulfillmentStatus'])
                 self.sale.setFee(sale['totalMarketplaceFee']['value'])
                 self.sale.add()
 
@@ -77,8 +77,8 @@ class getSales:
                     self.line.setTitle(line_items['title'])
                     self.line.setSaleFormat(line_items['soldFormat'])
                     self.line.setQuantity(line_items['quantity'])
-                    self.line.setAddressStatus(line_items
-                        ['lineItemAddressStatus']
+                    self.line.setFulfillmentStatus(line_items
+                        ['lineItemFulfillmentStatus']
                     )
                     self.line.add()
 
@@ -126,7 +126,7 @@ class getSales:
                     if not self.refund.alreadyExists():
                         self.refund.add()
 
-                for shipping in sale['AddressStartInstructions']:
+                for shipping in sale['fulfillmentStartInstructions']:
                     ship_to = shipping['shippingStep']['shipTo']
                     self.address.setBuyerName(ship_to['fullName'])
                     self.address.setAddressLine1(ship_to['contactAddress']
@@ -145,4 +145,3 @@ class getSales:
                     self.address.setCountryCode(ship_to['contactAddress']
                         ['countryCode']
                     )
-                    self.address.add()